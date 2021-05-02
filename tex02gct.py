58c4e3469017e7ec4d01433559ae1923f6016706
itsmeft24 <57544858+itsmeft24@users.noreply.github.com>
2021-05-02 17:46:38 -0400

    rgba8 support LETS GOOOO

--- tex02gct.py
+++ tex02gct.py
@@ -7,6 +7,7 @@ tex0 = open(sys.argv[1], "rb")
 gct = open(sys.argv[2], "wb")
 
 CMPR_HEADER = b"\x00\x00\x00\x02\x00\x00\x00\x29\x00\x00\x00\x00"
+RGBA8_HEADER = b"\x00\x00\x00\x02\x00\x00\x00\x0F\x00\x00\x00\x00"
 CI8_HEADER =  b"\x00\x00\x00\x02\x00\x00\x00\x3A"
 
 tex0.seek(0x20)
@@ -24,6 +25,26 @@ if pixelformat == 0x0e:
     tex0.seek(0x40)
     data = tex0.read()
     gct.write(CMPR_HEADER)
+    area = int((width*height)/2)
+    gct.write(number_images.to_bytes(4, byteorder='big'))
+    gct.write(width.to_bytes(4, byteorder='big'))
+    gct.write(height.to_bytes(4, byteorder='big'))
+    gct.write(width.to_bytes(4, byteorder='big'))
+    gct.write(height.to_bytes(4, byteorder='big'))
+    gct.write(area.to_bytes(4, byteorder='big'))
+    gct.write(data)
+elif pixelformat == 0x06:
+    tex0.seek(0x8)
+    version_number = int.from_bytes(tex0.read(4), "big")
+    tex0.seek(0x24)
+    number_images = int.from_bytes(tex0.read(4), "big")
+    tex0.seek((version_number*4)+0x18)
+    width = int.from_bytes(tex0.read(2), "big")
+    tex0.seek((version_number*4)+0x1a)
+    height = int.from_bytes(tex0.read(2), "big")
+    tex0.seek(0x40)
+    data = tex0.read()
+    gct.write(RGBA8_HEADER)
     area = int((width*height)/2)
     gct.write(number_images.to_bytes(4, byteorder='big'))
     gct.write(width.to_bytes(4, byteorder='big'))
