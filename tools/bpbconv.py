c216736b8985a78f2b6a37c42f4bb1be63a4a840
itsmeft24 <57544858+itsmeft24@users.noreply.github.com>
2022-01-15 15:56:31 -0500

    add comments and stuff

--- tools/bpbconv.py
+++ tools/bpbconv.py
@@ -1,15 +1,19 @@
 import sys, struct, zlib, io, numpy as np
 
-#STRUCT_DEFINITIONS
+if len(sys.argv) < 3:
+    print("Cars .BPB (BinaryParameterBlock) to .PB (ParameterBlock) Converter\n\nUsage: python bpbconv.py <INPUT_BPB> <OUTPUT_FILE>")
+    exit()
+
+# STRUCT DEFINITIONS
 HEADER = ">4siihhiiii"
-PARAM_ENTRY = ">IIHHi"
+PARENT_DEFINITION = ">IIHHI"
 
 def readString(file):
     string = b""
     while True:
         str = file.read(1)
         if str == b'\x00':
-            return string.decode('ascii')
+            return string.decode(encoding="ascii", errors='ignore')
         string = string + str
 
 def djb_hash(str):
@@ -50,6 +54,8 @@ else:
     exit(-1)
 
 output = open(sys.argv[2], "w")
+
+# Read the string tables and add entries into a dictionary for cleaner lookups.
 param_block.seek(labelstrings_offset)
 
 hash_labels = {}
@@ -64,26 +70,28 @@ for x in range(len(labels)):
 del labels
 
 for x in range(numlabels):
-    param_block.seek(x*16+0x20)#Seek to the offset of the definition and unpack it into a tuple.
+    param_block.seek(x*16+0x20) # Seek to the offset of the definition and unpack it into a tuple
 
-    entry_data = struct.unpack(PARAM_ENTRY, param_block.read(0x10))
-    param_block.seek(x*16+0x20)
+    entry_data = struct.unpack(PARENT_DEFINITION, param_block.read(0x10))
 
-    HASH = entry_data[0]
+    hash = entry_data[0] # This format uses DJB-2 hashes to speed up parameter lookups
     string_offset = entry_data[1]
     flag = entry_data[2]
     number_of_children = entry_data[3]
     first_option_index = entry_data[4]
     
-    name = hash_labels[HASH]
+    try:
+        name = hash_labels[hash]
+    except:
+        name = hex(hash)
     
-    if flag == 65535:# Is an indexed Parent?
-        if x == 0: # Check if its not the first loop iteration, and add a newline at the beginning of the parent.
+    if flag == 65535:
+        if x == 0: # Check if its not the first loop iteration, and add a newline at the beginning of the parent
             output.write("["+name+"]\n")
         else:
             output.write("\n["+name+"]\n")
-    else:
-        if x == 0: # Check if its not the first loop iteration, and add a newline at the beginning of the parent.
+    else: # Append parent suffix (typically an index)
+        if x == 0: # Check if its not the first loop iteration, and add a newline at the beginning of the parent
             output.write("["+name+str(flag)+"]\n")
         else:
             output.write("\n["+name+str(flag)+"]\n")
@@ -92,76 +100,69 @@ for x in range(numlabels):
     for y in range(number_of_children):
         
         param_block.seek(offset_seek+y*16)
-        entry_data = struct.unpack(PARAM_ENTRY, param_block.read(0x10))
-        HASH = entry_data[0]
-        string_offset = entry_data[1]
 
-        param_block.seek(offset_seek+4+y*16)
-        extra_string_offset = param_block.read(1)
-        if extra_string_offset != 0:
-            param_block.seek(offset_seek+5+y*16)
-            string_offset = struct.unpack('>I', b"\x00"+param_block.read(3))[0]
+        hash = int.from_bytes(param_block.read(4), "big") # This format uses DJB-2 hashes to speed up parameter lookups
+        ror_flag = int.from_bytes(param_block.read(1), "big") # Only used in Race-O-Rama BPBs
+        string_offset = int.from_bytes(param_block.read(3), "big")
+        # This might cause some problems later down the road, but unlikely. If the game can only use 24 bits for the string offset, then this should be fine.
+        # Or, both the flag and string offset are actually 16 bits and im just crazy!
         
-        flag = entry_data[2]
-        value_offset = entry_data[4]
-        name = hash_labels[HASH]
+        mn_flag = int.from_bytes(param_block.read(2), "big", signed=False) # Flag in MN, child suffix in ror
+        reserved = param_block.read(2) # null unless a string is stored right inside the parent definition
+        value_buf = param_block.read(4) # Can be an offset into the value string table, an int, or a float. its parsed into its appropriate type in the following if block.
+        
+        try:
+            name = hash_labels[hash]
+        except:
+            name = hex(hash) # Used as a fallback, useful if we ever try modifying ror's save data
+        
         param_block.seek(offset_seek+y*16)
         
         if RACE_O_RAMA:
-            if flag == 65535:
-                OPTION_INDEX = ""
+            if mn_flag == 65535:
+                pass
             else:
-                OPTION_INDEX = str(flag)
+                name+=str(mn_flag) # Append child suffix (typically an index)
             
-            if extra_string_offset == b"\x40":
-                param_block.seek(0xA, 1)
+            if ror_flag == 0x00: # Offset into value string table
+                param_block.seek(int.from_bytes(value_buf, "big", signed=False)+optionstrings_offset)
                 value = readString(param_block)
                 if " " in value:
-                    output.write(name+OPTION_INDEX+"="+'"'+value+'"\n')
+                    output.write(name+"="+'"'+value+'"\n')
                 else:
-                    output.write(name+OPTION_INDEX+"="+value+"\n")
-            elif extra_string_offset == b"\x80":
-                param_block.seek(0xC, 1)
-                value = str(struct.unpack('>i', param_block.read(4))[0])
-                output.write(name+OPTION_INDEX+"="+value+"\n")
-            elif extra_string_offset == b"\xC0":
-                param_block.seek(0xC, 1)
-                value = str(np.float32(struct.unpack('>f', param_block.read(4))[0]))
-                output.write(name+OPTION_INDEX+"="+value+"\n")
-            elif extra_string_offset == b"\x00":
-                param_block.seek(value_offset+optionstrings_offset)
-                value = readString(param_block)
+                    output.write(name+"="+value+"\n")
+            elif ror_flag == 0x40: # String stored inside the child definition
+                value = (reserved + value_buf).replace(b"\x00", b"").decode(encoding="ascii", errors='ignore')
                 if " " in value:
-                    output.write(name+OPTION_INDEX+"="+'"'+value+'"\n')
+                    output.write(name+"="+'"'+value+'"\n')
                 else:
-                    output.write(name+OPTION_INDEX+"="+value+"\n")
+                    output.write(name+"="+value+"\n")
+            elif ror_flag == 0x80: # Integer
+                output.write(name+"="+str(int.from_bytes(value_buf, "big", signed=True))+"\n")
+            elif ror_flag == 0xC0: # Float
+                value = str(np.float32(struct.unpack('>f', value_buf)[0])) # Truncate float using NumPy
+                output.write(name+"="+value+"\n")
             else:
-                print("UNKNOWN_FLAG of type RACE_O_RAMA: "+str(extra_string_offset)+" Please report this file to @data.arc#5576 on Discord!")
+                print("Unknown Race-O-Rama type flag!: "+hex(ror_flag)+" Please open an issue on the https://www.github.com/itsmeft24/cars-research repository! (Be sure to attach the file!)")
         else:
-        
-            if flag == 65535:
-                print("Flag 0xFF found! Please report this file to @data.arc#5576 on Discord!")
-            elif flag == 1:# String Offset
-                param_block.seek(value_offset+optionstrings_offset)
+            if mn_flag == 0x01: # Offset into value string table
+                param_block.seek(int.from_bytes(value_buf, "big", signed=False)+optionstrings_offset)
                 value = readString(param_block)
                 if " " in value:
                     output.write(name+"="+'"'+value+'"\n')
                 else:
                     output.write(name+"="+value+"\n")
-            elif flag == 2:# Flag is 0x02?
-                param_block.seek(0xA, 1)
-                value = readString(param_block)
+            elif mn_flag == 0x02: # String stored inside the child definition
+                value = (reserved + value_buf).replace(b"\x00", b"").decode(encoding="ascii", errors='ignore')
                 if " " in value:
                     output.write(name+"="+'"'+value+'"\n')
                 else:
                     output.write(name+"="+value+"\n")
-            elif flag == 8:# Flag is 0x08?
-                param_block.seek(0xC, 1)
-                value = str(np.float32(struct.unpack('>f', param_block.read(4))[0]))
-                output.write(name+"="+value+"\n")
-            elif flag == 4:# Flag is 0x04?
-                param_block.seek(0xC, 1)
-                value = str(struct.unpack('>i', param_block.read(4))[0])
+            elif mn_flag == 0x04: # Integer
+                output.write(name+"="+str(int.from_bytes(value_buf, "big", signed=True))+"\n")
+            elif mn_flag == 0x08: # Float
+                value = str(np.float32(struct.unpack('>f', value_buf)[0])) # Truncate float using NumPy
                 output.write(name+"="+value+"\n")
             else:
-                print(name+" UNKNOWN_FLAG:"+hex(flag)+" Please report this file to @itsmeft24#5576 on Discord!")
+                print("Unknown Mater-National type flag!: "+mn_flag+" Please open an issue on the https://www.github.com/itsmeft24/cars-research repository! (Be sure to attach the file!)")
+output.close()
